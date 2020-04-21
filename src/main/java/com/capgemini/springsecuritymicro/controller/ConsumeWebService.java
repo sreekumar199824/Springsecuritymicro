b395fc2596d072035278d7986a7d088258fe832e
sreekumar199824 <sreekumar199824@gmail.com>
2020-04-21 20:14:13 +0530

    SpringSecurity final commit

--- src/main/java/com/capgemini/springsecuritymicro/controller/ConsumeWebService.java
+++ src/main/java/com/capgemini/springsecuritymicro/controller/ConsumeWebService.java
@@ -32,7 +32,7 @@ public class ConsumeWebService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<UserInfoBean> entity = new HttpEntity<UserInfoBean>(bean,headers);
-		return restTemplate.exchange("http://3.22.205.236:8081/updateMan",HttpMethod.POST,entity,ResponseClass.class).getBody();
+		return restTemplate.exchange("http://13.59.58.190:8081/updateMan",HttpMethod.POST,entity,ResponseClass.class).getBody();
 	}
 	
 	@RequestMapping(value = "/template/getAllMans",method = RequestMethod.GET)
@@ -40,7 +40,7 @@ public class ConsumeWebService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<ResponseClass> entity = new HttpEntity<ResponseClass>(headers);
-		return restTemplate.exchange("http://3.22.205.236:8081/getAllMans",HttpMethod.GET,entity,ResponseClass.class).getBody();
+		return restTemplate.exchange("http://13.59.58.190:8081/getAllMans",HttpMethod.GET,entity,ResponseClass.class).getBody();
 	}
 	
 	@RequestMapping(value = "/template/deleteMan",method = RequestMethod.GET)
@@ -48,7 +48,7 @@ public class ConsumeWebService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<ResponseClass> entity = new HttpEntity<ResponseClass>(headers);
-		return restTemplate.exchange("http://3.22.205.236:8081/deleteMan?userId="+userId,HttpMethod.GET,entity,ResponseClass.class).getBody();
+		return restTemplate.exchange("http://13.59.58.190:8081/deleteMan?userId="+userId,HttpMethod.GET,entity,ResponseClass.class).getBody();
 	}
 	
 
@@ -57,7 +57,7 @@ public class ConsumeWebService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<UserInfoBean> entity = new HttpEntity<UserInfoBean>(bean,headers);
-		return restTemplate.exchange("http://3.22.205.236:8084/buyProduct",HttpMethod.POST,entity,ResponseClass.class).getBody();
+		return restTemplate.exchange("http://13.59.58.190:8084/buyProduct",HttpMethod.POST,entity,ResponseClass.class).getBody();
 	}
 	
 	@RequestMapping(value = "/template/getOrder",method = RequestMethod.GET)
@@ -65,7 +65,7 @@ public class ConsumeWebService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<ResponseClass> entity = new HttpEntity<ResponseClass>(headers);
-		return restTemplate.exchange("http://3.22.205.236:8084/getOrder?orderId="+orderId,HttpMethod.GET,entity,ResponseClass.class).getBody();
+		return restTemplate.exchange("http://13.59.58.190:8084/getOrder?orderId="+orderId,HttpMethod.GET,entity,ResponseClass.class).getBody();
 	}
 	
 	@RequestMapping(value = "/template/getDealerProds",method = RequestMethod.GET)
@@ -73,7 +73,7 @@ public class ConsumeWebService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<ResponseClass> entity = new HttpEntity<ResponseClass>(headers);
-		return restTemplate.exchange("http://3.22.205.236:8084/getDealerProds",HttpMethod.GET,entity,ResponseClass.class).getBody();
+		return restTemplate.exchange("http://13.59.58.190:8084/getDealerProds",HttpMethod.GET,entity,ResponseClass.class).getBody();
 	}
 	
 	@RequestMapping(value = "/template/dealerOrder",method = RequestMethod.POST)
@@ -81,7 +81,7 @@ public class ConsumeWebService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<UserInfoBean> entity = new HttpEntity<UserInfoBean>(bean,headers);
-		return restTemplate.exchange("http://3.22.205.236:8083/dealerOrder",HttpMethod.POST,entity,ResponseClass.class).getBody();
+		return restTemplate.exchange("http://13.59.58.190:8083/dealerOrder",HttpMethod.POST,entity,ResponseClass.class).getBody();
 	}
 	
 	@RequestMapping(value = "/template/setSellingPrice",method = RequestMethod.POST)
@@ -89,7 +89,7 @@ public class ConsumeWebService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<DealerProductInfoBean> entity = new HttpEntity<DealerProductInfoBean>(bean,headers);
-		return restTemplate.exchange("http://3.22.205.236:8083/setSellingPrice",HttpMethod.POST,entity,ResponseClass.class).getBody();
+		return restTemplate.exchange("http://13.59.58.190:8083/setSellingPrice",HttpMethod.POST,entity,ResponseClass.class).getBody();
 	}
 	
 	@RequestMapping(value = "/template/getProds",method = RequestMethod.GET)
@@ -97,7 +97,7 @@ public class ConsumeWebService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<ResponseClass> entity = new HttpEntity<ResponseClass>(headers);
-		return restTemplate.exchange("http://3.22.205.236:8083/getProds?userId="+userId,HttpMethod.GET,entity,ResponseClass.class).getBody();
+		return restTemplate.exchange("http://13.59.58.190:8083/getProds?userId="+userId,HttpMethod.GET,entity,ResponseClass.class).getBody();
 	}
 	
 	@RequestMapping(value = "/template/getProd",method = RequestMethod.GET)
@@ -105,7 +105,7 @@ public class ConsumeWebService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<ResponseClass> entity = new HttpEntity<ResponseClass>(headers);
-		return restTemplate.exchange("http://3.22.205.236:8083/getProd?dealersProductId="+dealersProductId,HttpMethod.GET,entity,ResponseClass.class).getBody();
+		return restTemplate.exchange("http://13.59.58.190:8083/getProd?dealersProductId="+dealersProductId,HttpMethod.GET,entity,ResponseClass.class).getBody();
 	}
 	
 	
@@ -115,7 +115,7 @@ public class ConsumeWebService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<DealerProductInfoBean> entity = new HttpEntity<DealerProductInfoBean>(bean,headers);
-		return restTemplate.exchange("http://3.22.205.236:8083/updateDealerProduct",HttpMethod.POST,entity,ResponseClass.class).getBody();
+		return restTemplate.exchange("http://13.59.58.190:8083/updateDealerProduct",HttpMethod.POST,entity,ResponseClass.class).getBody();
 	}
 	
 	@RequestMapping(value = "/template/setMin",method = RequestMethod.POST)
@@ -123,7 +123,7 @@ public class ConsumeWebService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<DealerProductInfoBean> entity = new HttpEntity<DealerProductInfoBean>(bean,headers);
-		return restTemplate.exchange("http://3.22.205.236:8083/setMin",HttpMethod.POST,entity,ResponseClass.class).getBody();
+		return restTemplate.exchange("http://13.59.58.190:8083/setMin",HttpMethod.POST,entity,ResponseClass.class).getBody();
 	}
 	
 	
@@ -132,7 +132,7 @@ public class ConsumeWebService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<ResponseClass> entity = new HttpEntity<ResponseClass>(headers);
-		return restTemplate.exchange("http://3.22.205.236:8083/getMansProds",HttpMethod.GET,entity,ResponseClass.class).getBody();
+		return restTemplate.exchange("http://13.59.58.190:8083/getMansProds",HttpMethod.GET,entity,ResponseClass.class).getBody();
 	}
 	
 	@RequestMapping(value = "/template/addprod",method = RequestMethod.POST)
@@ -140,7 +140,7 @@ public class ConsumeWebService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<UserInfoBean> entity = new HttpEntity<UserInfoBean>(bean,headers);
-		return restTemplate.exchange("http://3.22.205.236:8082/addprod",HttpMethod.POST,entity,ResponseClass.class).getBody();
+		return restTemplate.exchange("http://13.59.58.190:8082/addprod",HttpMethod.POST,entity,ResponseClass.class).getBody();
 	}
 	
 
@@ -149,7 +149,7 @@ public class ConsumeWebService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<ProductInfoBean> entity = new HttpEntity<ProductInfoBean>(bean,headers);
-		return restTemplate.exchange("http://3.22.205.236:8082/updateCost",HttpMethod.PUT,entity,ResponseClass.class).getBody();
+		return restTemplate.exchange("http://13.59.58.190:8082/updateCost",HttpMethod.PUT,entity,ResponseClass.class).getBody();
 	}
 	
 	@RequestMapping(value = "/template/getAllProducts",method = RequestMethod.GET)
@@ -157,7 +157,7 @@ public class ConsumeWebService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<ResponseClass> entity = new HttpEntity<ResponseClass>(headers);
-		return restTemplate.exchange("http://3.22.205.236:8082/getAllProducts?userId="+userId,HttpMethod.GET,entity,ResponseClass.class).getBody();
+		return restTemplate.exchange("http://13.59.58.190:8082/getAllProducts?userId="+userId,HttpMethod.GET,entity,ResponseClass.class).getBody();
 	}
 	
 	@RequestMapping(value = "/template/updateProd",method = RequestMethod.PUT)
@@ -165,7 +165,7 @@ public class ConsumeWebService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<ProductInfoBean> entity = new HttpEntity<ProductInfoBean>(bean,headers);
-		return restTemplate.exchange("http://3.22.205.236:8082/updateProd",HttpMethod.PUT,entity,ResponseClass.class).getBody();
+		return restTemplate.exchange("http://13.59.58.190:8082/updateProd",HttpMethod.PUT,entity,ResponseClass.class).getBody();
 	}
 	
 	@RequestMapping(value = "/template/getPayments",method = RequestMethod.GET)
@@ -173,7 +173,7 @@ public class ConsumeWebService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<ResponseClass> entity = new HttpEntity<ResponseClass>(headers);
-		return restTemplate.exchange("http://3.22.205.236:8082/getPayments?userId="+userId,HttpMethod.GET,entity,ResponseClass.class).getBody();
+		return restTemplate.exchange("http://13.59.58.190:8082/getPayments?userId="+userId,HttpMethod.GET,entity,ResponseClass.class).getBody();
 	}
 	
 	@RequestMapping(value = "/template/removeProd",method = RequestMethod.GET)
@@ -181,7 +181,7 @@ public class ConsumeWebService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<ResponseClass> entity = new HttpEntity<ResponseClass>(headers);
-		return restTemplate.exchange("http://3.22.205.236:8082/removeProd?productId="+productId,HttpMethod.GET,entity,ResponseClass.class).getBody();
+		return restTemplate.exchange("http://13.59.58.190:8082/removeProd?productId="+productId,HttpMethod.GET,entity,ResponseClass.class).getBody();
 	}
 	
 }
